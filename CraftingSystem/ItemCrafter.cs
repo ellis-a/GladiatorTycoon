6dfdf606ea2fc8ecf22d3346c2aaf1fa7edb7513
ellis-a <andr3w3llis@gmail.com>
2022-07-06 19:21:05 +1000

    committing everything before moving pc

---
+++ CraftingSystem/ItemCrafter.cs
@@ -0,0 +1,26 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftingSystem
+{
+    public class ItemCrafter
+    {
+        public Item CraftWeapon(List<Item> ingredients, int skill, WeaponType weaponType)
+        {
+
+
+            return new Item();
+        }
+    }
+
+    enum WeaponType
+    {
+        Sword,
+        Axe,
+        Hammer
+    }
+}
